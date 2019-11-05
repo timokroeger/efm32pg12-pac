57c6e0dff0bb1a939cb17f243ca40314795143b1
Timo Kröger <timokroeger93@gmail.com>
2019-11-05 17:33:18 +0100

    Fix typos which resulted in missing peripherals

--- src/lib.rs
+++ src/lib.rs
@@ -553,6 +553,24 @@ impl Deref for WTIMER0 {
 }
 #[doc = "WTIMER0"]
 pub mod wtimer0;
+#[doc = "WTIMER1"]
+pub struct WTIMER1 {
+    _marker: PhantomData<*const ()>,
+}
+unsafe impl Send for WTIMER1 {}
+impl WTIMER1 {
+    #[doc = r"Returns a pointer to the register block"]
+    #[inline(always)]
+    pub const fn ptr() -> *const wtimer0::RegisterBlock {
+        0x4001_a400 as *const _
+    }
+}
+impl Deref for WTIMER1 {
+    type Target = wtimer0::RegisterBlock;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*WTIMER1::ptr() }
+    }
+}
 #[doc = "USART0"]
 pub struct USART0 {
     _marker: PhantomData<*const ()>,
@@ -707,6 +725,42 @@ impl Deref for PCNT0 {
 }
 #[doc = "PCNT0"]
 pub mod pcnt0;
+#[doc = "PCNT1"]
+pub struct PCNT1 {
+    _marker: PhantomData<*const ()>,
+}
+unsafe impl Send for PCNT1 {}
+impl PCNT1 {
+    #[doc = r"Returns a pointer to the register block"]
+    #[inline(always)]
+    pub const fn ptr() -> *const pcnt0::RegisterBlock {
+        0x4004_e400 as *const _
+    }
+}
+impl Deref for PCNT1 {
+    type Target = pcnt0::RegisterBlock;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*PCNT1::ptr() }
+    }
+}
+#[doc = "PCNT2"]
+pub struct PCNT2 {
+    _marker: PhantomData<*const ()>,
+}
+unsafe impl Send for PCNT2 {}
+impl PCNT2 {
+    #[doc = r"Returns a pointer to the register block"]
+    #[inline(always)]
+    pub const fn ptr() -> *const pcnt0::RegisterBlock {
+        0x4004_e800 as *const _
+    }
+}
+impl Deref for PCNT2 {
+    type Target = pcnt0::RegisterBlock;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*PCNT2::ptr() }
+    }
+}
 #[doc = "I2C0"]
 pub struct I2C0 {
     _marker: PhantomData<*const ()>,
@@ -727,6 +781,24 @@ impl Deref for I2C0 {
 }
 #[doc = "I2C0"]
 pub mod i2c0;
+#[doc = "I2C1"]
+pub struct I2C1 {
+    _marker: PhantomData<*const ()>,
+}
+unsafe impl Send for I2C1 {}
+impl I2C1 {
+    #[doc = r"Returns a pointer to the register block"]
+    #[inline(always)]
+    pub const fn ptr() -> *const i2c0::RegisterBlock {
+        0x4000_c400 as *const _
+    }
+}
+impl Deref for I2C1 {
+    type Target = i2c0::RegisterBlock;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*I2C1::ptr() }
+    }
+}
 #[doc = "ADC0"]
 pub struct ADC0 {
     _marker: PhantomData<*const ()>,
@@ -767,6 +839,24 @@ impl Deref for ACMP0 {
 }
 #[doc = "ACMP0"]
 pub mod acmp0;
+#[doc = "ACMP1"]
+pub struct ACMP1 {
+    _marker: PhantomData<*const ()>,
+}
+unsafe impl Send for ACMP1 {}
+impl ACMP1 {
+    #[doc = r"Returns a pointer to the register block"]
+    #[inline(always)]
+    pub const fn ptr() -> *const acmp0::RegisterBlock {
+        0x4000_0400 as *const _
+    }
+}
+impl Deref for ACMP1 {
+    type Target = acmp0::RegisterBlock;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*ACMP1::ptr() }
+    }
+}
 #[doc = "IDAC0"]
 pub struct IDAC0 {
     _marker: PhantomData<*const ()>,
@@ -887,6 +977,24 @@ impl Deref for WDOG0 {
 }
 #[doc = "WDOG0"]
 pub mod wdog0;
+#[doc = "WDOG1"]
+pub struct WDOG1 {
+    _marker: PhantomData<*const ()>,
+}
+unsafe impl Send for WDOG1 {}
+impl WDOG1 {
+    #[doc = r"Returns a pointer to the register block"]
+    #[inline(always)]
+    pub const fn ptr() -> *const wdog0::RegisterBlock {
+        0x4005_2400 as *const _
+    }
+}
+impl Deref for WDOG1 {
+    type Target = wdog0::RegisterBlock;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*WDOG1::ptr() }
+    }
+}
 #[doc = "ETM"]
 pub struct ETM {
     _marker: PhantomData<*const ()>,
@@ -980,6 +1088,8 @@ pub struct Peripherals {
     pub TIMER1: TIMER1,
     #[doc = "WTIMER0"]
     pub WTIMER0: WTIMER0,
+    #[doc = "WTIMER1"]
+    pub WTIMER1: WTIMER1,
     #[doc = "USART0"]
     pub USART0: USART0,
     #[doc = "USART1"]
@@ -996,12 +1106,20 @@ pub struct Peripherals {
     pub CRYOTIMER: CRYOTIMER,
     #[doc = "PCNT0"]
     pub PCNT0: PCNT0,
+    #[doc = "PCNT1"]
+    pub PCNT1: PCNT1,
+    #[doc = "PCNT2"]
+    pub PCNT2: PCNT2,
     #[doc = "I2C0"]
     pub I2C0: I2C0,
+    #[doc = "I2C1"]
+    pub I2C1: I2C1,
     #[doc = "ADC0"]
     pub ADC0: ADC0,
     #[doc = "ACMP0"]
     pub ACMP0: ACMP0,
+    #[doc = "ACMP1"]
+    pub ACMP1: ACMP1,
     #[doc = "IDAC0"]
     pub IDAC0: IDAC0,
     #[doc = "VDAC0"]
@@ -1014,6 +1132,8 @@ pub struct Peripherals {
     pub RTCC: RTCC,
     #[doc = "WDOG0"]
     pub WDOG0: WDOG0,
+    #[doc = "WDOG1"]
+    pub WDOG1: WDOG1,
     #[doc = "ETM"]
     pub ETM: ETM,
     #[doc = "SMU"]
@@ -1079,6 +1199,9 @@ impl Peripherals {
             WTIMER0: WTIMER0 {
                 _marker: PhantomData,
             },
+            WTIMER1: WTIMER1 {
+                _marker: PhantomData,
+            },
             USART0: USART0 {
                 _marker: PhantomData,
             },
@@ -1103,13 +1226,25 @@ impl Peripherals {
             PCNT0: PCNT0 {
                 _marker: PhantomData,
             },
+            PCNT1: PCNT1 {
+                _marker: PhantomData,
+            },
+            PCNT2: PCNT2 {
+                _marker: PhantomData,
+            },
             I2C0: I2C0 {
+                _marker: PhantomData,
+            },
+            I2C1: I2C1 {
                 _marker: PhantomData,
             },
             ADC0: ADC0 {
                 _marker: PhantomData,
             },
             ACMP0: ACMP0 {
+                _marker: PhantomData,
+            },
+            ACMP1: ACMP1 {
                 _marker: PhantomData,
             },
             IDAC0: IDAC0 {
@@ -1128,6 +1263,9 @@ impl Peripherals {
                 _marker: PhantomData,
             },
             WDOG0: WDOG0 {
+                _marker: PhantomData,
+            },
+            WDOG1: WDOG1 {
                 _marker: PhantomData,
             },
             ETM: ETM {
